3e1b8da08a653a53ffa75c6229be8c94ad4d3110
Brad Choate <brad@bradchoate.com>
2017-04-05 16:54:03 -0700

    Changes to rebuild script and fix for newrelic config.

--- setup/linode-rebuild.sh
+++ setup/linode-rebuild.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+if [ ! -z "$(git status --untracked-files=no --porcelain)" ]; then
+    echo "You have uncommitted changes in this branch."
+    exit 1
+fi
+
 # StackScript for "MLTSHP Web Node" - 77953
 STACKSCRIPT="MLTSHP Web Node"
 
 # Our NodeBalancer label
 NODEBALANCER_NAME="mltshp-web-cluster"
+
+# Docker image to deploy
+DOCKER_IMAGE_NAME=${1:-mltshp/mltshp-web:latest}
 
 # Prefix for all nodes attached to the NodeBalancer
 NODE_PREFIX="mltshp-web"
@@ -15,6 +23,7 @@ PUBLIC_KEY="setup/mltshp-web-key.pub"
 echo "This script will rebuild all active MLTSHP web nodes"
 echo "using the latest Docker image."
 echo
+echo "Docker image to deploy: $DOCKER_IMAGE_NAME"
 echo "Linode NodeBalancer: $NODEBALANCER_NAME"
 echo "Web Nodes to rebuild: ${NODE_PREFIX}-*"
 echo "StackScript to deploy: $STACKSCRIPT"
@@ -51,7 +60,7 @@ function rebuild_node() {
         --distribution "Ubuntu 16.04 LTS" \
         --pubkey-file $PUBLIC_KEY \
         --stackscript "$STACKSCRIPT" \
-        --stackscriptjson '{}' \
+        --stackscriptjson '{"DOCKER_IMAGE_NAME": "$DOCKER_IMAGE_NAME"}' \
         --password "$(dd bs=32 count=1 if="/dev/urandom" 2>/dev/null | base64 | tr +/ _.)"
 
     echo -n "Waiting for node availability..."
@@ -79,11 +88,31 @@ function rebuild_node() {
     echo
 }
 
+function slackpost {
+    # Usage: slackpost <channel> <message>
+    # Requires SLACK_WEBOOK_URL environment variable to be set
+    # to a valid incoming webhook URL
+
+    if [ -n "$SLACK_WEBHOOK_URL" ]; then
+        channel=$1
+        shift
+        text=$*
+
+        escapedText=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
+        json="{\"channel\": \"#$channel\", \"icon_emoji\": \":mltshp:\", \"text\": \"$escapedText\"}"
+        curl -s -d "payload=$json" "$SLACK_WEBHOOK_URL" > /dev/null
+    fi
+}
+
+slackpost "technology" "MLTSHP deployment starting for Docker image $DOCKER_IMAGE_NAME..."
+
 # Get a list of nodes from the cluster
 nodes=$( linode nodebalancer --action node-list --label "$NODEBALANCER_NAME" --port 80 | grep -oE $NODE_PREFIX-\\d+ | tail -r )
 for node in $nodes;
 do
     rebuild_node $node
 done
+
+slackpost "technology" "Docker image $DOCKER_IMAGE_NAME deployed to production."
 
 echo "All nodes rebuilt!"
