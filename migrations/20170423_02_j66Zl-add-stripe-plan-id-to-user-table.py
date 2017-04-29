ef8d17c989241c9d96716761e4cbbd899d4d0b73
Brad Choate <brad@bradchoate.com>
2017-04-29 11:42:08 -0700

    Migration for stripe plan column.

---
+++ migrations/20170423_02_j66Zl-add-stripe-plan-id-to-user-table.py
@@ -0,0 +1,11 @@
+"""
+Add stripe_plan_id column to user table
+"""
+
+from yoyo import step
+
+__depends__ = {'20170423_01_TwSED-adds-a-migration-state-table'}
+
+steps = [
+    step("ALTER TABLE `user` ADD COLUMN `stripe_plan_id` VARCHAR(40) NULL AFTER `stripe_customer_id`")
+]
