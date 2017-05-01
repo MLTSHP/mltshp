395a9673080a722447c1c540fe78c35907f5c403
Brad Choate <brad@bradchoate.com>
2017-05-01 00:18:46 -0700

    Make sure migration tasks are registered

--- celeryconfig.example.py
+++ celeryconfig.example.py
@@ -1,5 +1,5 @@
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ("tasks.timeline", "tasks.counts")
+CELERY_IMPORTS = ("tasks.timeline", "tasks.counts", "tasks.migration")
 
 ## Result store settings.
 CELERY_RESULT_BACKEND = "rpc://"
