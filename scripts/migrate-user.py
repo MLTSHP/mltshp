0ceb3b755ff598145ec00b99f8c471c06fd9f3a6
Brad Choate <brad@bradchoate.com>
2017-05-17 22:56:51 -0700

    Task script udpates

--- scripts/migrate-user.py
+++ scripts/migrate-user.py
@@ -5,10 +5,14 @@ from tornado.options import options
 from models import User
 from tasks.migration import migrate_for_user
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 def main():
     names = sys.argv[2:]
 
+    options.use_workers = False
     for name in names:
         user = User.get("name=%s and deleted=2", name)
         if user is not None:
