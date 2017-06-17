a60001990b2a8d7a8d4abfe677eb2551c5baf31e
Brad Choate <brad@bradchoate.com>
2017-06-17 16:10:38 -0700

    Use mltshp_testing database name for mysql image

--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -8,9 +8,10 @@ docker tag mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER} mltshp/mltshp-web:l
 mkdir -p /tmp/buildkite-mltshp-mysql
 docker-compose -f .buildkite/docker-compose.yml up -d
 docker exec -t buildkite_mltshp_1 ./run-tests.sh
-rm -rf /tmp/buildkite-mltshp-mysql
 
 # submit coverage data
 docker exec -t -e BUILDKITE -e BUILDKITE_JOB_ID -e BUILDKITE_BRANCH -e COVERALLS_REPO_TOKEN buildkite_mltshp_1 ./coveralls-report.sh
 
 docker-compose -f .buildkite/docker-compose.yml down
+
+rm -rf /tmp/buildkite-mltshp-mysql
