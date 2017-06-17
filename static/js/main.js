1192c359a65c0c72ae7cea84bc629fe3ee5f2954
Scott Vandehey <(none)>
2017-06-16 22:48:07 -0700

    Convert Remaining Buttons to Button Component
    This commit converts all remaining buttons to use the standard button
    CSS component, ensuring that all buttons use the same styles.
    
    It also removes the focus ring for buttons, since we know these buttons
    have sufficently distinct hover/focus styles for keyboard users.
    
    fixes #203
    fixes #211
    fixes #212

--- static/js/main.js
+++ static/js/main.js
@@ -317,7 +317,7 @@ $(document).ready(function() {
     // Inline editing of the title.
     $(".image-edit-title-form .cancel").click(function() {
       $(this).closest('.image-title').find(".image-edit-title").show();
-      $(this).closest(".image-edit-title-form").hide();
+      $(this).closest(".image-edit-title-form").removeClass('is-active');
       return false;
     });
 
@@ -336,7 +336,7 @@ $(document).ready(function() {
         if ('title_raw' in result) {
           $(that).hide();
           $title_container.find('.title-input').val(result['title_raw']);
-          $(that).next(".image-edit-title-form").show();
+          $(that).next(".image-edit-title-form").addClass('is-active');
         }
       }, 'json');
 
@@ -351,7 +351,7 @@ $(document).ready(function() {
           var $title_container = $(that).closest('.image-title');
           $title_container.find('.image-edit-title').html(result['title']).show();
           $title_container.find('.title-input').val(result['title_raw']);
-          $title_container.find('.image-edit-title-form').hide();
+          $title_container.find('.image-edit-title-form').removeClass('is-active');
         }
       }, 'json');
       return false;
@@ -1551,7 +1551,7 @@ $(document).ready(function() {
         select_user: function(user_name) {
           this.clear_results();
           $input_field.val(user_name);
-          $invite_button.removeClass('invite-button-inactive');
+          $invite_button.removeAttr('disabled');
         },
 
         submit_form: function(ev) {
@@ -1570,11 +1570,11 @@ $(document).ready(function() {
 
         clear_input: function() {
           $input_field.val('');
-          $invite_button.addClass('invite-button-inactive');
+          $invite_button.attr('disabled', 'disabled');
         },
 
         send_invite: function() {
-          if ($invite_button.hasClass('invite-button-inactive')) {
+          if ($invite_button.disabled) {
             return false;
           } else {
             var url = $form.attr('action');
