8cffe862c60138aa83b8ea44247341da1595268f
Brad Choate <brad@bradchoate.com>
2017-06-17 15:47:10 -0700

    Use existing images to speed build.

--- .buildkite/steps/build-web.sh
+++ .buildkite/steps/build-web.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+# pull prior image to populate layer cache
+docker pull mltshp/mltshp-web:latest
+
 docker build -t mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER} .
 docker push mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER}
--- .buildkite/steps/build-worker.sh
+++ .buildkite/steps/build-worker.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+# pull prior image to populate layer cache
+docker pull mltshp/mltshp-worker:latest
+
 docker build -t mltshp/mltshp-worker:build-${BUILDKITE_BUILD_NUMBER} -f Dockerfile.worker .
 docker push mltshp/mltshp-worker:build-${BUILDKITE_BUILD_NUMBER}
