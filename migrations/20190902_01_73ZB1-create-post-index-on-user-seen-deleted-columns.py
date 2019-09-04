c0ecc37ee11f2e236f8666ebd8b10f98a3996b3b
Brad Choate <brad@bradchoate.com>
2019-09-03 19:10:25 -0500

    Adjusting query for timeline select to use proper index (#544)
    * Adjusting query for timeline select to use proper index
    
    * Catching up db schema files to match current schema.

--- migrations/20190902_01_73ZB1-create-post-index-on-user-seen-deleted-columns.py
+++ migrations/20190902_01_73ZB1-create-post-index-on-user-seen-deleted-columns.py
@@ -7,5 +7,5 @@ from yoyo import step
 __depends__ = {'20190901_01_6HEw7-adds-a-fulltext-index-to-the-sharedfile-table'}
 
 steps = [
-    step("ALTER TABLE post ADD INDEX userseendeletedid_idx (user_id, seen, deleted, sharedfile_id);")
+    step("ALTER TABLE post ADD INDEX userseendeletedid_idx (user_id, seen, deleted, sharedfile_id) USING BTREE;")
 ]
