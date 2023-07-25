79998d6dc81c2cb5c1adedc93ab24a6684c2bca6
Brad Choate <brad@bradchoate.com>
2023-07-24 21:48:28 -0500

    Updates to Buildkite steps

--- .buildkite/steps/build-worker.sh
+++ .buildkite/steps/build-worker.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# exit if any command fails (e); strict variable substitution (u);
+# set exit code to non-zero for any failed piped commands (o pipefail)
+# See also: http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-# pull prior image to populate layer cache
-docker pull mltshp/mltshp-worker:latest
+echo "--- Pulling base Docker image"
+    docker pull mltshp/mltshp-worker:latest
 
-docker build -t mltshp/mltshp-worker:build-${BUILDKITE_BUILD_NUMBER} -f Dockerfile.worker .
-docker push mltshp/mltshp-worker:build-${BUILDKITE_BUILD_NUMBER}
+echo "+++ Building Docker image for worker node"
+    docker build -t mltshp/mltshp-worker:build-${BUILDKITE_BUILD_NUMBER} -f Dockerfile.worker .
+
+echo "--- Pushing build Docker image to Docker Hub"
+    docker push mltshp/mltshp-worker:build-${BUILDKITE_BUILD_NUMBER}
--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exit if any command fails (e); strict variable substitution (u);
+# set exit code to non-zero for any failed piped commands (o pipefail)
+# See also: http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
 wait_for() {
@@ -7,24 +10,24 @@ wait_for() {
     while ! nc -z $1 $2; do echo sleeping; sleep 2; done
 }
 
-docker pull mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER}
-docker tag mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER} mltshp/mltshp-web:latest
+echo "--- Pulling base Docker image"
+    docker pull mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER}
+    docker tag mltshp/mltshp-web:build-${BUILDKITE_BUILD_NUMBER} mltshp/mltshp-web:latest
 
-# launch fakes3/mysql/web app
-docker compose -f .buildkite/docker-compose.yml up -d --build
+echo "--- Launching Docker containers"
+    docker compose -f .buildkite/docker-compose.yml up -d --build
 
-# let's wait and allow mysql/fakes3 to spin up
-#wait_for localhost 3306
-#wait_for localhost 8000
-sleep 10
+echo "~~~ Waiting for containers to start"
+    #wait_for localhost 3306
+    #wait_for localhost 8000
+    sleep 10
 
-# run our tests against it
-docker exec -t buildkite_mltshp_1 ./run-tests.sh
+echo "+++ Running unit tests"
+    docker exec -t buildkite_mltshp_1 ./run-tests.sh
 
-# submit coverage data
-docker exec -t -e BUILDKITE -e BUILDKITE_JOB_ID -e BUILDKITE_BRANCH -e COVERALLS_REPO_TOKEN buildkite_mltshp_1 ./coveralls-report.sh
+echo "--- Submitting coverage data"
+    docker exec -t -e BUILDKITE -e BUILDKITE_JOB_ID -e BUILDKITE_BRANCH -e COVERALLS_REPO_TOKEN buildkite_mltshp_1 ./coveralls-report.sh
 
-# tear down containers
-docker compose -f .buildkite/docker-compose.yml down
-
-docker container prune -f
+echo "~~~ Stopping containers; cleanup"
+    docker compose -f .buildkite/docker-compose.yml down
+    docker container prune -f
