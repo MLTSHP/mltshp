03dbfe9a543bc90ca9a60702c91c6c2e40bdcb3a
Brad Choate <brad@bradchoate.com>
2025-01-04 19:32:53 -0600

    Updating various env shebang lines to reference python3
    Caused a problem with the celery tasks, but changing everything to be
    consistent.

--- scripts/make-zip-of-images.py
+++ scripts/make-zip-of-images.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Create a zip file of all the images posted or shared from 
 an account, zip into a file on S3, and email notification to them.
