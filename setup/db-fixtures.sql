c366753168552a5b106eba68b1d515b8da609d6b
James Beard <james@smallsaucepan.com>
2025-10-25 06:49:03 +1100

    Changed pretty_date to handle empty input argument (#851)
    * Added an empty arg check to pretty_date. Should probably never happen, though was getting an fatal error for what should be a minor data quality issue.
    
    * Based on PR feedback removed default None value (function typically
    expects one). Renamed function parameter 'time' to avoid any clashes
    with time std module. Added created_at value to admin user in test
    database to avoid original issue.

--- setup/db-fixtures.sql
+++ setup/db-fixtures.sql
@@ -3,8 +3,8 @@
 -- auth_secret for local testing: dummy-secret
 INSERT INTO `user` (`name`, `disable_notifications`, `created_at`) VALUES ('mltshp', 1, now());
 -- password for "admin" is "password" if you're using the local testing auth_secret key
-INSERT INTO `user` (`name`, `hashed_password`, `email`, `full_name`, `email_confirmed`, `is_paid`, `stripe_plan_id`)
-     VALUES ('admin', '9bbdccf408a2420e20fcd157c6315d5f77427c64', 'admin@example.com', 'Site Admin', 1, 1, 'mltshp-double');
+INSERT INTO `user` (`name`, `hashed_password`, `email`, `full_name`, `email_confirmed`, `is_paid`, `stripe_plan_id`, `created_at`)
+     VALUES ('admin', '9bbdccf408a2420e20fcd157c6315d5f77427c64', 'admin@example.com', 'Site Admin', 1, 1, 'mltshp-double', now());
 
 INSERT INTO `shake` (`user_id`, `type`, `name`) VALUES (1, 'user', 'Best of MLTSHP');
 INSERT INTO `shake` (`user_id`, `type`) VALUES (2, 'user');
